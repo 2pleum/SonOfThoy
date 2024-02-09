f1d04fb1cbce3aa21f3ac5ab86970e0793ff2ec4
2pleum <kantida.mails@gmail.com>
2024-02-09 21:06:57 +0700

    +update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
-    public static void setFrame() {
+
+    public static void main(String[] args) {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         JFrame frame = new JFrame();
         frame.getContentPane().setLayout(null);
@@ -10,8 +13,9 @@ public class Main {
         frame.setSize(screenSize.getSize());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        JLabel text = new JLabel("Hello world",JLabel.LEFT);
+        text.setSize(100,100);
+        frame.add(text);
     }
-    public static void main(String[] args) {
-        setFrame();
-    }
+
 }
--- src/ourButton.java
+++ src/ourButton.java
@@ -3,20 +3,14 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ourButton implements ActionListener {
-    private Button a = new Button();
-    private String img, name;
-    private float size;
-    public ourButton(String img,String name,float size) {
+public class ourButton {
+    private String img;
+    private int w, h;
+    public ourButton(String img,int w,int h) {
         this.img = img;
-        this.name = name;
-        this.size = size;
+        this.w = w;
+        this.h = h;
     }
-    public void setButton() {
 
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
-    }
 }
