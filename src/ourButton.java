8aa761c010cfebd236869d13980452af53b56726
2pleum <kantida.mails@gmail.com>
2024-02-09 19:39:24 +0700

    +update

--- src/ourButton.java
+++ src/ourButton.java
@@ -1,2 +1,22 @@
-public class ourButton {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ourButton implements ActionListener {
+    private Button a = new Button();
+    private String img, name;
+    private float size;
+    public ourButton(String img,String name,String color,float size) {
+        this.img = img;
+        this.name = name;
+        this.size = size;
+    }
+    public void setButton() {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
