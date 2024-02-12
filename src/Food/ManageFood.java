07811b4ed68292f273e01578548443dabcc25791
2pleum <kantida.mails@gmail.com>
2024-02-12 13:50:13 +0700

    +add object HeathyFood

--- src/Food/ManageFood.java
+++ src/Food/ManageFood.java
@@ -12,7 +12,47 @@ public class ManageFood {
 
     }
     public static HealthyFood [] setHealthyFood() {
-
+        HealthyFood [] h = new HealthyFood[40];
+        h[0] = new HealthyFood("สลัดอกไก่");
+        h[1] = new HealthyFood("ข้าวยำสเต๊กไก่แซ่บสูตรคลีน");
+        h[2] = new HealthyFood("ข้าวฟักทองผัดไข่");
+        h[3] = new HealthyFood("แกงจืดเต้าหู้หมูสับ");
+        h[4] = new HealthyFood("สเต๊กอกไก่");
+        h[5] = new HealthyFood("สเต๊กปลาแซลมอน");
+        h[6] = new HealthyFood("สลัดกุ้ง");
+        h[7] = new HealthyFood("สุกี้ไก่");
+        h[8] = new HealthyFood("สุกี้หมู");
+        h[9] = new HealthyFood("สุกี้ปลา");
+        h[10] = new HealthyFood("สลัดไข่ต้ม");
+        h[11] = new HealthyFood("แซนวิซอาโวคาโด้ไข่ต้ม");
+        h[12] = new HealthyFood("ข้าวไรซ์เบอร์รี่ผัดกะเพราอกไก่");
+        h[13] = new HealthyFood("ข้าวมะเขือเทศผัดไข่");
+        h[14] = new HealthyFood("ข้าวแตงกวาผัดไข่");
+        h[15] = new HealthyFood("ผัดบวบผัดไข่");
+        h[16] = new HealthyFood("สลัดทูน่า่");
+        h[17] = new HealthyFood("สลัดผักออแกนิค");
+        h[18] = new HealthyFood("เมี่ยงปลาเผา");
+        h[19] = new HealthyFood("ข้าวไรซ์เบอร์รี่ปลาย่างซอสมะนาว");
+        h[20] = new HealthyFood("ซีซาร์สลัด");
+        h[21] = new HealthyFood("ซุปฟักทองและไข่ต้ม");
+        h[22] = new HealthyFood("อาซาอิโบล์ว");
+        h[23] = new HealthyFood("ข้าวต้มกุ้งเห็ดหอม");
+        h[24] = new HealthyFood("โจ๊กไก่เห็ดหอม");
+        h[25] = new HealthyFood("ข้าวไรซ์เบอร์รี่ไก่ย่างน้ำจิ้มแจ๋ว");
+        h[26] = new HealthyFood("สลัดอาโวคาโด");
+        h[27] = new HealthyFood("สลัดโรล");
+        h[28] = new HealthyFood("สุกี้โรล");
+        h[29] = new HealthyFood("ลวกจิ๋มอกไก่");
+        h[30] = new HealthyFood("สลัดผลไม้");
+        h[31] = new HealthyFood("อกไก่ผัดบาร์บีคิว");
+        h[32] = new HealthyFood("ยำปลากระป๋องไข่ต้ม");
+        h[33] = new HealthyFood("ยำทูน่าไข่ต้ม");
+        h[34] = new HealthyFood("ผักต้มน้ำพริกอ่อง");
+        h[35] = new HealthyFood("ผัดผักรวมมิตร");
+        h[36] = new HealthyFood("ปลากระพงย่าง");
+        h[37] = new HealthyFood("ข้าวผัดธัญพืช5สี");
+        h[38] = new HealthyFood("ก๋วยเตี๋ยวลุยสวน");
+        h[39] = new HealthyFood("ลาบอกไก่");
     }
     public static WesternFood [] setWesternFood() {
 
--- src/SunOfThoy/Main.java
+++ src/SunOfThoy/Main.java
@@ -1,3 +1,5 @@
+package SunOfThoy;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
