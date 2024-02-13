4ff86640a2e1cc1d427b870dddc3aa130ff11d54
cheekkkii <gamsasivimon@gmail.com>
2024-02-13 16:59:27 +0700

    P

--- src/Main.java
+++ src/Main.java
@@ -451,13 +451,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        JFrame frame = new JFrame();
-        frame.getContentPane().setLayout(null);
-        frame.setVisible(true);
-        frame.setSize(screenSize.getSize());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
+//        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+//        JFrame frame = new JFrame();
+//        frame.getContentPane().setLayout(null);
+//        frame.setVisible(true);
+//        frame.setSize(screenSize.getSize());
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setLocationRelativeTo(null);
+        MainFrame mainFrame = new MainFrame();
     }
 
 }
