8d2a12d59371f2ba722abbbabab4a65572714ad3
cheekkkii <gamsasivimon@gmail.com>
2024-02-18 01:04:04 +0700

    ~ checkout new branch CHEEK

--- com.SonOfThoy.app/ui/ButtonToStart.java
+++ com.SonOfThoy.app/ui/ButtonToStart.java
@@ -1,29 +1,25 @@
 package ui;
-
 import scenes.MainFrame;
 import scenes.MenuFrame;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class ButtonToStart extends JButton implements ActionListener {
     private final MainFrame mainFrame;
-
+    private final ImageIcon image;
     public ButtonToStart(MainFrame mainFrame) {
-          this.mainFrame = mainFrame;
-          ImageIcon image = new ImageIcon("img/1.png");
-          this.setIcon(image);
-//        ImageIcon xxx = new ImageIcon("startB.gif");
-
-//        ImageIcon xxx = new ImageIcon("path/to/startB.gif");
-//        this.setIcon(xxx);
-//        this.setRolloverIcon(xxx);
-
-//        this.setPressedIcon(xxx);
-          this.setPreferredSize(new Dimension(250, 250)); // ปรับขนาดเป็น 150x30
-          this.addActionListener(this);
+        this.mainFrame = mainFrame;
+        image = new ImageIcon("img/S__4571152.jpg");
+        this.setIcon(image); // ตั้งรูปภาพให้กับปุ่ม
+        this.setPreferredSize(new Dimension(300, 300)); // ปรับขนาดเป็น 250x250
+        this.setOpaque(false);
+        this.setContentAreaFilled(false);
+        this.setBorderPainted(false);
+        this.setBorder(BorderFactory.createEmptyBorder()); // ลบขอบของปุ่ม
+        this.addActionListener(this); // เพิ่ม ActionListener เพื่อจัดการเหตุการณ์การคลิกปุ่ม
 
     }
 
@@ -39,4 +35,13 @@ public class ButtonToStart extends JButton implements ActionListener {
             menuframe.setVisible(true); // เรียกใช้ setVisible(true) เพื่อให้ scenes.Menuframe ปรากฏบนหน้าจอ
         }
     }
+
+    public float getIconHeight(){
+        return image.getIconHeight();
+    }
+
+    public float getIconWidth(){
+        return image.getIconWidth();
+    }
+
 }
