74f9d90a007fd15ac66ca2fe4293dfc7834a2270
cheekkkii <gamsasivimon@gmail.com>
2024-02-29 14:18:10 +0700

    ~ checkout new branch CHEEK

--- com.SonOfThoy.app/scenes/LastFrame.java
+++ com.SonOfThoy.app/scenes/LastFrame.java
@@ -3,7 +3,6 @@ package scenes;
 import ui.LastLabel;
 import ui.exitButton;
 import ui.goHomeButton;
-import ui.lastL2;
 
 import javax.swing.*;
 import java.awt.*;
@@ -26,12 +25,11 @@ public class LastFrame extends JFrame {
         nameFood.setBounds((this.getWidth() - (nameFood.getPreferredSize().width)+5) / 2, this.getHeight()/2 - 230+370, nameFood.getPreferredSize().width+5, nameFood.getPreferredSize().height+20);
         this.add(nameFood);
 
-        lastL2 lastL2 = new lastL2(this);
-        JLabel thoy = lastL2.getThoy();
+        JLabel thoy = lastL.getThoySay();
         thoy.setBounds(((this.getWidth() - (thoy.getPreferredSize().width)+5) / 2)+140, this.getHeight()/2 - 230, thoy.getPreferredSize().width, thoy.getPreferredSize().height+20);
         this.add(thoy);
 
-        JLabel thoy2 = lastL2.getThoy2();
+        JLabel thoy2 = lastL.getThoySay2();
         thoy2.setBounds(((this.getWidth() - (thoy2.getPreferredSize().width)+5) / 2)+140, this.getHeight()/2 - 230+50, thoy2.getPreferredSize().width, thoy2.getPreferredSize().height+20);
         this.add(thoy2);
 
--- com.SonOfThoy.app/ui/LastLabel.java
+++ com.SonOfThoy.app/ui/LastLabel.java
@@ -9,7 +9,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class LastLabel {
-    private final JLabel nameFood;
+    private JLabel nameFood,thoySay,thoySay2;
     private String name;
     public LastLabel(LastFrame lastF,String name) {
         this.name = name;
@@ -23,12 +23,41 @@ public class LastLabel {
             e.printStackTrace();
             // จัดการข้อผิดพลาด
         }
+        thoySay = new JLabel("วันนี้พี่ตอย");
+        try {
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("img/Myfont/ZF#2ndPixelus.ttf")).deriveFont(80f);
+            thoySay.setFont(customFont);
+            thoySay.setForeground(new Color(0x7C5A5A));
+
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+            // จัดการข้อผิดพลาด
+        }
+        //
+        thoySay2 = new JLabel("ขอนำเสนอเมนู");
+        try {
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("img/Myfont/ZF#2ndPixelus.ttf")).deriveFont(80f);
+            thoySay2.setFont(customFont);
+            thoySay2.setForeground(new Color(0x7C5A5A));
+
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+            // จัดการข้อผิดพลาด
+        }
+
 
 
     }
 
-    public JLabel getNameFood() {
+
+    public JLabel getNameFood(){
         return nameFood;
+    }
+    public JLabel getThoySay() {
+        return thoySay;
+    }
+    public JLabel getThoySay2() {
+        return thoySay2;
     }
 
 }
